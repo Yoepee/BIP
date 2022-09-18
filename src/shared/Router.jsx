1c675a5f5b794e51bb97b09706030d088a6d950b
Yoepee <kdy2975@naver.com>
2022-09-18 21:52:56 +0900

    기본 라우터, 리덕스 세팅

---
+++ src/shared/Router.jsx
@@ -0,0 +1,9 @@
+import { Route, Routes } from "react-router-dom"
+
+const Router = () =>{
+    return (
+        <Routes>
+            <Route path="/" element={<div>헬로</div>}/>
+        </Routes>
+    )
+}
