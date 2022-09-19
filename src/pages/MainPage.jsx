f239a66bc42d0acf2fdde0ff338c3b2c18e30667
Yoepee <kdy2975@naver.com>
2022-09-19 19:21:24 +0900

    메인페이지 크로셀 세팅

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,43 @@
+import Carousel from 'react-material-ui-carousel'
+import { Paper, Button } from '@mui/material'
 const MainPage = () => {
+    var items = [
+        {
+            name: "Random Name #1",
+            description: "Probably the most random thing you have ever seen!"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!"
+        },
+        {
+            name: "Random Name #3",
+            description: "Hello World!"
+        }
+    ]
+
     return (
-        <div></div>
+        <Carousel>
+            {
+                items.map( (item, i) => <Item key={i} item={item} /> )
+            }
+        </Carousel>
+            )
+}
+
+function Item(props)
+{
+    return (
+        <div>
+        <Paper>
+            <h2>{props.item.name}</h2>
+            <p>{props.item.description}</p>
+
+            <Button className="CheckButton">
+                테스트
+            </Button>
+        </Paper>
+    </div>
     )
 }
 
