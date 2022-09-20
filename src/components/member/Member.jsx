f2f97042cb8dfd610c2cfa7b8364090f5754216a
Yoepee <kdy2975@naver.com>
2022-09-20 13:40:46 +0900

    추가 : 친구목록 페이지 생성

---
+++ src/components/member/Member.jsx
@@ -0,0 +1,33 @@
+const Member = () =>{
+  return (
+    <div>
+      <div style={{border:"1px solid black", margin:"10px"}}>
+      <div style={{display:"flex", borderBottom:"1px solid black"}}>
+        친구
+        </div>
+        <div style={{display:"flex", borderBottom:"1px solid black"}}>
+          <p>사진</p>
+          <p>이름</p>
+        </div>
+        <div style={{display:"flex", borderBottom:"1px solid black"}}>
+          <p>사진</p>
+          <p>이름</p>
+        </div>
+        <div style={{display:"flex", borderBottom:"1px solid black"}}>
+          <p>사진</p>
+          <p>이름</p>
+        </div>
+        <div style={{display:"flex", borderBottom:"1px solid black"}}>
+          <p>사진</p>
+          <p>이름</p>
+        </div>
+        <div style={{display:"flex"}}>
+          <p>사진</p>
+          <p>이름</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Member;
