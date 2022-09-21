f65469a566322721a325ecdd220ec1ac510d773e
Yoepee <kdy2975@naver.com>
2022-09-22 02:55:42 +0900

    수정 : 이메일 유효성 검사 검증 및 서버 연결

--- src/components/signup/CheckEmail.jsx
+++ src/components/signup/CheckEmail.jsx
@@ -1,32 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TextField } from "@material-ui/core";
 import styled from "styled-components";
 import { Button } from "@material-ui/core";
 
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import { useNavigate } from "react-router-dom";
-
-
-
-
-
+import axios from "axios";
 const CheckEmail = () => {
   const [visble, setVisble] = useState(false);
   const [chkBtn,setChkBtn] = useState("인증하기 받기")
 	const navigate = useNavigate();
 	
   const initialState = {
-    email:''
+    value:''
   }
-  const [member, setMember] = useState(initialState)
+  const [member, setMember] = useState(initialState);
+  const [chkmail,setChkmail] = useState("사용 가능한 이메일 입니다.")
+  const [test,setTest] = useState("");
   /** 이메일 주소 유효검사*/ 
   const regexEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+  const regtest = /^[0-9]{6}$/;
 
   const onChangeHandler = (e) => {
     const {name, value} = e.target;
     setMember({...member, [name]: value})
   }
 
+  const __chkEmail = async(payload)=>{
+    let a = await axios.post(process.env.REACT_APP_SERVER_HOST+"/api/member/chkemail", payload);
+  }
+
+  const __examEmail = async(payload)=>{
+    let a = await axios.post(process.env.REACT_APP_SERVER_HOST+"/api/member/auth/email", payload)
+    .then((response)=>{
+      console.log(response)
+      setChkmail(response.data.data)
+      setVisble(!visble);
+    });
+  }
+
+  useEffect(()=>{
+    if(regexEmail.test(member.value)){
+      __chkEmail(member);
+    }
+  },[member])
+
+  const __emailLogin = async (payload) => {
+    let a = await axios.post(process.env.REACT_APP_SERVER_HOST + "/api/member/login/email", payload)
+    .then((response)=>{
+      if(response.data.success===true){
+        localStorage.setItem("Authorization", response.headers.authorization);
+        localStorage.setItem("RefreshToken", response.headers.refreshtoken);
+        // localStorage.setItem("name", response.data.data);
+        navigate("/")
+      }else{
+       alert(response.data.data) 
+      }
+    });
+  }
   return (
     <div>
       <Wrapper>
@@ -38,23 +69,25 @@ const CheckEmail = () => {
 
         <InfoArea>
           <p>
-            이메일 인증이 필요합니다.
+            이메일로 계정찾기
           </p>
         </InfoArea>
           <form action="">
           <TextField
             id="outlined-basic"
-            name="email"
-            value={member.email}
+            name="value"
+            value={member.value}
             onChange={onChangeHandler}
             label="이메일"
             variant="outlined"
             placeholder="이메일을 입력해주세요"
 						
           />
-           {member.email===""?null: regexEmail.test(member.email)?(<div style={{color: "#00766c", fontSize:"14px"}}>사용가능한 이메일입니다.</div>):(<div style={{color:"red", fonSizen:"14px"}}>올바른 이메일이 아닙니다.</div>)}
+           {member.value===""?null: regexEmail.test(member.value)?null:(<div style={{color:"red", fonSizen:"14px"}}>올바른 이메일 형식이 아닙니다.</div>)}
 
-          {visble && <TextField variant="outlined" label="인증번호" placeholder="인증번호를 입력해주세요" />}
+           {visble && <TextField variant="outlined" label="인증번호" placeholder="인증번호를 입력해주세요" value={test} onChange={(e)=>{setTest(e.target.value)}} minLength={6} maxLength={6}/>}
+          {test === "" ? null :
+            regtest.test(test) ? null : (<><div style={{ color: "red", fonSizen: "14px" }}>6자리 인증번호를 입력해주세요.</div></>)}
 					
           </form>
           
@@ -66,14 +99,20 @@ const CheckEmail = () => {
           <Button
             variant="contained" 
             onClick={() => {
-              setVisble(!visble); 
-              if(!visble){
-                setChkBtn("인증번호 확인하기")
-              }else{
-                setChkBtn("인증하기 받기")
-              }
-              if(chkBtn==="인증번호 확인하기"){
-                navigate("/signup/email/nickname")
+              if (regexEmail.test(member.value)) {
+                // if (regexEmail.test(member.value)) {
+                if (!visble) {
+                  setChkBtn("인증번호 확인하기");
+                  __examEmail(member);
+                } else {
+                  if(regtest.test(test)){
+                    __chkEmail(member);
+                  }else{
+                    alert("인증번호를 확인해주세요.")
+                  }
+                }
+              } else {
+                alert("이메일을 확인해주세요.")
               }
             }}>
             {chkBtn}
