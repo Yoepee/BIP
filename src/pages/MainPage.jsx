212b840230fe4855457b153cbd61dd85b3f00ea4
jamie7dev <jamie7dev@gmail.com>
2022-09-21 13:40:28 +0900

    수정: calendar 파일들 폴더 이동

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import Carousel from 'react-material-ui-carousel'
 import { Paper, Button } from '@mui/material'
-import Month from "../components/main/Month"
-import Day from "../components/main/Day"
+import Month from "../components/main/calendar/Month"
+import Day from "../components/main/calendar/Day"
 import styled from "styled-components";
-import Weekdate from "../components/main/Weekdate";
+import Weekdate from "../components/main/calendar/Weekdate";
 import Footer from "../components/footer/Footer";
-import Test from "../components/main/Test";
+import Test from "../components/main/calendar/Test";
 
 const MainPage = () => {
   const [settings, setSettings] = useState({autoPlay: false})
