79ade3326c5c7bcd36103d9fbbbc6b736652037b
Yoepee <kdy2975@naver.com>
2022-09-25 02:25:14 +0900

    수정 : 멤버목록 서버에서 받아오기 수정 (친구추가 받아오는 목록 일치 필요, 별칭 보유자 식별 필요)

--- src/components/member/Member.jsx
+++ src/components/member/Member.jsx
@@ -17,9 +17,12 @@ const Member = () =>{
         {member?.data?.data?.map((friend, i)=>{
           return (
             <div style={{display:"flex", border:"1px solid black", margin:"10px"}} key={i}>
-            <p>/사진/</p>
+            {friend.profileImgUrl===null?
+            <img src={process.env.PUBLIC_URL + `/assets/user_svg.svg`} style={{width:"50px"}}/>
+            :<img src={friend.profileImgUrl}/>
+            }
             <p>{friend.nickname}</p>
-            <p>/수정할 이름/</p>
+            <p>({friend.nickname})</p>
             <p style={{marginLeft:"auto", marginRight:"2%"}}>{friend.creditScore}</p>
             </div>
           )
