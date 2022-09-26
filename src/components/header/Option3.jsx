d96949641035233807a8a1a5aca77b480014a005
Yoepee <kdy2975@naver.com>
2022-09-26 16:30:27 +0900

    수정 : 약속 수정 방장기능 별도로 편성

--- src/components/header/Option3.jsx
+++ src/components/header/Option3.jsx
@@ -1,12 +1,14 @@
 import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
 import { useNavigate, useParams } from 'react-router-dom';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ManageAccountsRoundedIcon from '@mui/icons-material/ManageAccountsRounded';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import zIndex from '@mui/material/styles/zIndex';
 
 const Option3 = ({ head }) => {
   const navigate = useNavigate();
-  const [chk, setChk] = useState(false);
+  const [chk, setChk] = useState(0);
   const [leader, setLeader] = useState(false);
   const { id } = useParams();
 
@@ -19,8 +21,8 @@ const Option3 = ({ head }) => {
     }).then((response) => {
       console.log(response);
       if (response.data.success) {
-        if(response.data.data.nickname === localStorage.getItem("name"))
-          setLeader(true)
+        if (response.data.data.nickname === localStorage.getItem("name"))
+          setLeader(true);
       } else {
         return;
       }
@@ -35,16 +37,15 @@ const Option3 = ({ head }) => {
           RefreshToken: localStorage.getItem('RefreshToken'),
         }
       }).then((response) => {
-        console.log(response)
         if (response.data.success) {
           navigate("/")
         } else {
           alert(response.data.data);
-          setChk(!chk);
+          setChk(0);
         }
       })
     } else {
-      setChk(!chk);
+      setChk(0);
     }
   }
 
@@ -56,16 +57,15 @@ const Option3 = ({ head }) => {
           RefreshToken: localStorage.getItem('RefreshToken'),
         }
       }).then((response) => {
-        console.log(response)
         if (response.data.success) {
           navigate("/")
         } else {
           alert(response.data.data);
-          setChk(!chk);
+          setChk(0);
         }
       })
     } else {
-      setChk(!chk);
+      setChk(0);
     }
   }
 
@@ -83,10 +83,39 @@ const Option3 = ({ head }) => {
       <div style={{ marginLeft: "1%" }}>
         <p>{head}</p>
       </div>
-      <div onClick={() => { setChk(!chk); }} style={{ marginLeft: "auto", marginRight: "2%" }}>
+      <div style={{ marginLeft: "auto", marginRight: "2%", display:"flex" }}>
+      {leader?
+      <div onClick={() => { if(chk===0){setChk(2);} else{setChk(0)} }} style={{  marginRight: "20px" }}>
+        <p><ManageAccountsRoundedIcon /></p>
+      </div>
+      :null}
+      {chk===2 ?
+        <div style={{
+          width: "150px",
+          position: "absolute",
+          backgroundColor: "white",
+          top: "50px", right: "60px",
+          textAlign: "center",
+          borderRadius: "5px",
+          border: "1px solid black",
+          zIndex:"10"
+        }}>
+          <div style={{ borderBottom: "1px solid black", padding: "3px", cursor: "pointer" }}
+            onClick={() => { console.log("아")}}>방장 위임</div>
+          <div style={{ borderBottom: "1px solid black", padding: "3px", cursor: "pointer" }}
+            onClick={() => { console.log("아")}}>멤버 추방</div>
+          <div style={{ borderBottom: "1px solid black", padding: "3px", cursor: "pointer" }}
+            onClick={() => { navigate(`/addpromise/edit${id}`) }}>약속 수정</div>
+          <div style={{ borderBottom: "1px solid black", padding: "3px", cursor: "pointer" }}
+            onClick={() => { removePromist() }}>약속 삭제</div>
+          <div style={{ padding: "3px", cursor: "pointer" }}
+            onClick={() => { setChk(0); }}>취소</div>
+        </div>
+        : null}
+      <div onClick={() => { if(chk===0){setChk(1);} else{setChk(0)} }} style={{  marginRight: "2%" }}>
         <p><MoreVertIcon /></p>
       </div>
-      {chk && !leader ?
+      {chk == 1?
         <div style={{
           width: "150px",
           position: "absolute",
@@ -94,33 +123,17 @@ const Option3 = ({ head }) => {
           top: "50px", right: "20px",
           textAlign: "center",
           borderRadius: "5px",
-          border: "1px solid black"
+          border: "1px solid black",
+          zIndex:"10"
         }}>
           <div style={{ borderBottom: "1px solid black", padding: "3px", cursor: "pointer" }}
             onClick={() => { exitPromist() }}>약속 나가기</div>
           <div style={{ padding: "3px", cursor: "pointer" }}
             onClick={() => { setChk(!chk); }}>취소</div>
         </div>
-        : chk && leader ? <div style={{
-          width: "150px",
-          position: "absolute",
-          backgroundColor: "white",
-          top: "50px", right: "20px",
-          textAlign: "center",
-          borderRadius: "5px",
-          border: "1px solid black"
-        }}>
-          <div style={{ borderBottom: "1px solid black", padding: "3px", cursor: "pointer" }}
-            onClick={() => { }}>방장 위임</div>
-          <div style={{ borderBottom: "1px solid black", padding: "3px", cursor: "pointer" }}
-            onClick={() => { navigate(`/addpromise/edit${id}`) }}>약속 수정</div>
-          <div style={{ borderBottom: "1px solid black", padding: "3px", cursor: "pointer" }}
-            onClick={() => { removePromist() }}>약속 삭제</div>
-          <div style={{ padding: "3px", cursor: "pointer" }}
-            onClick={() => { setChk(!chk); }}>취소</div>
-        </div>
-          : null
+        : null
       }
+      </div>
     </>
   )
 }
