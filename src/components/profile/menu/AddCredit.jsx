c51a99f96e59da0c0ca1cfc671dbdda637679a2f
Yoepee <kdy2975@naver.com>
2022-09-27 19:18:44 +0900

    추가 : 친구 신용도 올리는 기능 추가

--- src/components/profile/menu/AddCredit.jsx
+++ src/components/profile/menu/AddCredit.jsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const AddCredit = () =>{
+  const navigate = useNavigate();
   const __getMyCredit = async(num) =>{
     if(window.confirm(`신용점수를 ${num}점 구매하시겠습니까?`)){
-    let a = await axios.put(process.env.REACT_APP_SERVER_HOST+`/api/user/point`,{point:1000*num, nickname:localStorage.getItem("name")},{
+    let a = await axios.put(process.env.REACT_APP_SERVER_HOST+`/api/user/point`,{point:4000*num, nickname:localStorage.getItem("name")},{
       headers: {
           Authorization:localStorage.getItem('Authorization'),
           RefreshToken:localStorage.getItem('RefreshToken')
@@ -39,11 +41,13 @@ const AddCredit = () =>{
       <div style={{ display: "flex", border: "1px solid black", margin: "10px", alignItems:"center" }}>
       <p>신용점수 1점 구매</p>
         <AddBox onClick={()=>{
+          navigate("/member/add1");
         }}>선택</AddBox>
     </div>
     <div style={{ display: "flex", border: "1px solid black", margin: "10px", alignItems:"center" }}>
       <p>신용점수 5점 구매</p>
         <AddBox onClick={()=>{
+          navigate("/member/add5");
         }}>선택</AddBox>
     </div>
     </div>
