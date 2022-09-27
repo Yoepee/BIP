1bc7f788249b313ac85238849f169ff77d39b0ac
jamie7dev <jamie7dev@gmail.com>
2022-09-27 16:38:20 +0900

    수정: 메인페이지 약속 리스트 미디어쿼리 사용 PC 뷰 구현

--- src/components/main/list/PromiseList.jsx
+++ src/components/main/list/PromiseList.jsx
@@ -19,40 +19,44 @@ const PromiseList = ({day}) => {
         <div>
           <p style={{fontWeight:"bold"}}>약속 목록</p>
         </div>
-        {promiseList?.data?.data?.map((promise)=>{
-          return (
-            <PromiseCard key={promise.id} onClick={()=>{navigate(`/detailpromise/${promise.id}`)}}>
-              <div style={{display:"flex"}}>
-                <p style={{fontSize:"15px", fontWeight:"bold"}}>{promise.title}</p>
-                {/* 포인트 받아야할듯 */}
-                <People><PersonIcon/></People>
-                <p style={{fontWeight:"500"}}>{promise.memberCount}</p>
-              </div>
-              <p className="place">{promise.place}</p>
-              <div>
+        <Cards>
+          {promiseList?.data?.data?.map((promise)=>{
+            return (
+              <PromiseCard key={promise.id} onClick={()=>{navigate(`/detailpromise/${promise.id}`)}}>
                 <div style={{display:"flex"}}>
-                  {Number(promise.eventDateTime.split("-")[3])<12?
-                  <>
-                  <p>오전</p>
-                  <p>{promise.eventDateTime.split("-")[3]}시</p>
-                  </>
-                  :Number(promise.eventDateTime.split("-")[3])===12?
-                  <>
-                  <p>오후</p>
-                  <p>{promise.eventDateTime.split("-")[3]}시</p>
-                  </>
-                  :<>
-                  <p>오후</p>
-                  <p>{Number(promise.eventDateTime.split("-")[3])-12}시</p>
-                  </>
-                  }
-                  <p style={{marginRight:"10px"}}>{promise.eventDateTime.split("-")[4]}분</p>
-                  <p className="lastTime">{promise.lastTime}</p>
-                </div>             
-              </div>
-            </PromiseCard>
-          )
-        })}
+                  <p style={{fontSize:"15px", fontWeight:"bold"}}>{promise.title}</p>
+                  {/* 포인트 받아야할듯 */}
+                  <People><PersonIcon/></People>
+                  <p style={{fontWeight:"500"}}>{promise.memberCount}</p>
+                </div>
+                <div>
+                  <p className="place" style={{ marginTop:"0", marginBottom:"0" }}>{promise.place}</p>
+                </div>                
+                <div>
+                  <div style={{display:"flex"}}>
+                    {Number(promise.eventDateTime.split("-")[3])<12?
+                    <>
+                    <p>오전</p>
+                    <p>{promise.eventDateTime.split("-")[3]}시</p>
+                    </>
+                    :Number(promise.eventDateTime.split("-")[3])===12?
+                    <>
+                    <p>오후</p>
+                    <p>{promise.eventDateTime.split("-")[3]}시</p>
+                    </>
+                    :<>
+                    <p>오후</p>
+                    <p>{Number(promise.eventDateTime.split("-")[3])-12}시</p>
+                    </>
+                    }
+                    <p style={{marginRight:"10px"}}>{promise.eventDateTime.split("-")[4]}분</p>
+                    <p className="lastTime">{promise.lastTime}</p>
+                  </div>             
+                </div>
+              </PromiseCard>
+            )
+          })}
+        </Cards>        
       </Wrap>
     </>    
   )
@@ -65,6 +69,15 @@ const Wrap = styled.div`
   width: 80%;
   margin: 20px auto;
 `
+const Cards = styled.div`
+  @media screen and (min-width: 769px) {
+      /* background-color: pink; */
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 10px;
+    } 
+  
+`
 
 const PromiseCard = styled.div`
   /* min-width: 350px; */
@@ -72,18 +85,23 @@ const PromiseCard = styled.div`
   background-color: #EDFFEB;
   margin: 10px 0;
   padding: 0 10px;
-  border-radius: 4px;
-  font-size: 15px;
+  border-radius: 4px; 
+  font-size: 13px;
   position: relative;
+  div {
+    /* background-color: yellow; */
+    /* height: 30px; */
+  }
   .lastTime {
     position: absolute;
     right: 10px;
   }
   @media screen and (min-width: 769px) {
     background-color: #EDFFEB;
-    width: 200px;
-    height: 130px;
+    width: 92%;
+    height: 120px;
     font-size: 13px;
+    margin: 0 auto;
   }
 `
 
