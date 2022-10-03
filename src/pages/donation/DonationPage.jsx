d4d1371deaa321e847c2a672ec9bf3773682776c
Yoepee <kdy2975@naver.com>
2022-10-03 12:16:19 +0900

    추가 : 재능기부 경로 생성

---
+++ src/pages/donation/DonationPage.jsx
@@ -0,0 +1,46 @@
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import styled from "styled-components";
+import CreateIcon from '@mui/icons-material/Create';
+import { useNavigate } from "react-router-dom";
+
+const DonationPage = () =>{
+  const navigate = useNavigate();
+  return (
+    <>
+      <Header head={"재능기부"}/>
+      <p onClick={()=>{navigate("/detaildonation/1")}}>나는 기부페이지</p>
+      <Plus>
+          <IconBtn onClick={() => { navigate("/adddonation") }}><CreateIcon style={{color:"#6D09D1"}} /></IconBtn>
+      </Plus>
+      <Footer foot={3}/>
+      
+    </>
+  )
+}
+
+export default DonationPage;
+
+const Plus = styled.div`
+position : fixed;
+bottom : 0;
+right: 5%;
+margin-bottom:20%;
+`
+
+const IconBtn = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+border: 1.5px solid #6D09D1;
+border-radius:50%;
+background-color: white;
+width: 40px;
+height: 40px;
+color:white;
+cursor:pointer;
+//위치 고정시키기
+position: fixed;
+right: 10%;
+bottom: 15%;
+`
