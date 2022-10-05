8278b6807400808196678d359558f370da396d4d
tjsalsun@gmail.com <tjsalsun@gmail.com>
2022-10-06 02:50:19 +0900

    디자인 : 코멘트 Input창 ,코멘트 list 수정

--- src/components/footer/CommentFooter.jsx
+++ src/components/footer/CommentFooter.jsx
@@ -1,7 +1,9 @@
+import { Margin } from "@mui/icons-material";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { __addComment } from "../../redux/modules/comment";
+import styled from 'styled-components'
 
 const CommentFooter = () =>{
   const {id} = useParams();
@@ -33,14 +35,38 @@ const CommentFooter = () =>{
   }
 
   return (
-    <div style={{display:"flex"}}>
-      <input onKeyPress={handleKeyPress} placeholder="댓글 작성" name="content" value={comment.content} onChange={onChangeHandler}/>
-      <div
+    <div style={{display:"flex", marginTop:"10px"}}>
+      <div style={{width:"93%", backgroundColor:"#e9e9e9", borderRadius:"20px", height:"40px"}}>
+          <CommentInput  onKeyPress={handleKeyPress} placeholder="댓글 작성" name="content" value={comment.content} onChange={onChangeHandler}/>
+      </div>
+      
+      <CommentBtn 
       onClick={()=>{
         AddComment()
-      }}>작성</div>
+      }}>작성</CommentBtn>
     </div>
   )
 }
 
 export default CommentFooter;
+
+const CommentInput = styled.input`
+  width:80%; 
+  height: 35px;
+  margin-left:30px; 
+  outline: none;
+  border: none; 
+  border-radius: 10px;
+  background-color:#e9e9e9;
+`
+
+const CommentBtn = styled.div`
+  margin-left: 10px;
+  padding:0 3.5%;
+  width:35px; 
+  line-height: 35px;
+  border: 1px solid black;
+  border-radius: 20px;
+  font-size: 15px; 
+  
+`
