0b9e8ab08a4d2b4ae0af9e3c7f1739e5a915ef1d
jamie7dev <jamie7dev@gmail.com>
2022-10-11 12:49:57 +0900

    디자인: 웹헤더 추가

---
+++ src/components/header/WebHeader.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const WebHeader = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+    <StWebHeader>
+      <div onClick={() => { navigate("/") }}>홈</div>
+      <div onClick={() => { navigate("/donation") }}>재능기부</div>
+      <div onClick={() => { navigate("/member") }}>친구목록</div>
+      <div onClick={() => { navigate("/profile") }}>마이페이지</div>
+    </StWebHeader>    
+    </>
+  )
+}
+
+export default WebHeader;
+
+
+
+const StWebHeader = styled.div`
+  display: none;
+  @media screen and (min-width: 769px) {
+    /* background-color: pink; */
+    width: 420px;
+    display: flex;
+    margin: 15px 20px;
+    font-size: 14px;
+    font-weight: bold;
+    div {
+      margin: 0 10px;
+      cursor: pointer;
+    }
+  }
+`
--- src/pages/MemberPage.jsx
+++ src/pages/MemberPage.jsx
@@ -2,16 +2,20 @@ import { useState } from "react";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 import Member from "../components/member/Member";
+import WebHeader from "../components/header/WebHeader";
 
 const MemberPage = () =>{
   const [type, setType] = useState("none");
   return (
     <>
-    {type==="search"?
-    // 검색 기능시 헤더 변경 (헤더옵션 7)
-    <Header option={7} setType={setType}/>
-    // 평소 상태 헤더 옵션 4
-    :<Header head={"친구 목록"} option={4} setType={setType} type={type} />}
+    <div style={{display:"flex"}}>
+      <WebHeader />
+      {type==="search"?
+      // 검색 기능시 헤더 변경 (헤더옵션 7)
+      <Header option={7} setType={setType}/>
+      // 평소 상태 헤더 옵션 4
+      :<Header option={4} setType={setType} type={type} />}
+    </div>    
     {/* 친구목록 */}
     <Member type={type} setType={setType}/>
     {/* 푸터메뉴 2번째 항목 선택 (0,1,2,3) */}
