b032f0968ebdee7769d70074d6c2f10a1b0fa9a6
jamie7dev <jamie7dev@gmail.com>
2022-10-12 15:20:10 +0900

    디자인: 신용점수구매페이지 웹헤더 추가

--- src/pages/profile/AddCreditPage.jsx
+++ src/pages/profile/AddCreditPage.jsx
@@ -1,14 +1,16 @@
 import Header from "../../components/header/Header";
+import WebHeader from "../../components/header/WebHeader";
 import AddCredit from "../../components/profile/menu/AddCredit";
 
 const AddCreditPage = () => {
   return (
-    <div>
+    <>
+      <WebHeader />
       {/* 헤더 옵션 0 */}
       <Header head={"신용점수 구매"} option={0}/>
       {/* 신용점수 구매창 연결 (지정 값으로 작동) */}
       <AddCredit/>
-    </div>
+    </>
   )
 }
 export default AddCreditPage;
