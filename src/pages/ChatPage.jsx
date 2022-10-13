dd0777114188f6d54a317720753bc53c74fa7eec
jamie7dev <jamie7dev@gmail.com>
2022-10-13 13:26:45 +0900

    디자인: 채팅 뒤로가기 아이콘 변경

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -3,20 +3,31 @@ import { useNavigate } from "react-router-dom";
 import Chat from "../components/chat/Chat";
 import WebHeader from "../components/header/WebHeader";
 import { clearChat } from "../redux/modules/chat";
+import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
 
+import styled from "styled-components";
 
 const ChatPage = () =>{ 
   const navigate = useNavigate();
   const dispatch = useDispatch();
   return (
     <>
-    <WebHeader />
-    {/* 헤더 후에 수정필요 */}
-    <div onClick={() => { dispatch(clearChat());navigate("/") }}>홈으로</div>
-    {/* 채팅내용 불러오기(채팅 작성 footer식으로 빼도 됨) */}
-    <Chat/>
+    <div style={{backgroundColor:"#FAFAFA"}}>
+      <WebHeader />
+      {/* 헤더 후에 수정필요 */}
+      <BackIcon onClick={() => { dispatch(clearChat());navigate(-1) }}><ArrowBackIosNewRoundedIcon /></BackIcon>
+      {/* 채팅내용 불러오기(채팅 작성 footer식으로 빼도 됨) */}
+      <Chat/>
+    </div>
     </>
   )
 }
 
 export default ChatPage;
+
+const BackIcon = styled.div`
+  width: 24px;
+  padding-top: 26px;
+  padding-left: 26px;
+  cursor: pointer;
+`
