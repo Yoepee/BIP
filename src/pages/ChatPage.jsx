861fe97eb7b9fac80b8c9c3919016723af7f68dd
Yoepee <kdy2975@naver.com>
2022-10-21 14:30:27 +0900

    수정 : 채팅 모바일 헤더 약속 제목출력

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import Chat from "../components/chat/Chat";
 import WebHeader from "../components/header/WebHeader";
@@ -11,12 +11,18 @@ const ChatPage = () =>{
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const {id} = useParams();
+  const detail = useSelector((state)=>state.detailPromise);
+
+  console.log(detail)
   return (
     <>
     <div style={{backgroundColor:"#FAFAFA"}}>
       <WebHeader />
       {/* 헤더 후에 수정필요 */}
+      <div style={{display:"flex"}}>
       <BackIcon onClick={() => { dispatch(clearChat()); navigate(`/detailpromise/${id}`) }}><ArrowBackIosNewRoundedIcon /></BackIcon>
+      <BackIcon style={{width: "100%", cursor:"default"}}>{detail?.data?.data?.title}</BackIcon>
+      </div>
       {/* 채팅내용 불러오기(채팅 작성 footer식으로 빼도 됨) */}
       <Chat/>
     </div>
